2640cf95dc170cef45564cb777aabd4070dacfed
runnedrunner <auszhou@nuevaschool.org>
2016-11-16 09:03:44 -0800

    Classadoo autocommit

---
+++ -KW978vgwrFsExfH8SZj/script.js
@@ -0,0 +1,34 @@
+// demo function
+function logArgs(args) {
+  console.log(args)
+  // Get 
+}
+
+function coolfunction(args) {
+  return args;
+}
+
+$("#button" ).click(function () {
+  console.log("hi!");
+});
+var functionString = coolfunction.toString();
+console.log(functionString)
+var consoleLogExample = $("#cool-function-example")
+
+var formattedExample = $("<pre>")
+
+formattedExample.text(functionString);
+
+consoleLogExample.html(formattedExample);
+
+var functionString = logArgs.toString();
+console.log(functionString)
+var consoleLogExample = $("#console-log-example")
+
+var formattedExample = $("<pre>")
+
+formattedExample.text(functionString);
+
+consoleLogExample.html(formattedExample);
+
+var functi
